61e82de2e4dda9c82a25ccdd31069d2bc5554e82
tycinar80 <tycinar42@gmail.com>
2022-10-16 17:39:07 +0300

    Microservice Assignment

---
+++ src/test/java/tyc/microserviceassignment/MicroserviceassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package tyc.microserviceassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
